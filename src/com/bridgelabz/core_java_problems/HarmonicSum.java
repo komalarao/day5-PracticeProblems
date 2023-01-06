0ee666cdda96c3b3a34080ccc67206b71f3c6771
komalarao <manoharvakamullu1999@gmail.com>
2023-01-06 09:52:56 -0800

    added: program to find sum of harmonic numbers

---
+++ src/com/bridgelabz/core_java_problems/HarmonicSum.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.core_java_problems;
+import java.util.Scanner;
+
+public class HarmonicSum {
+		  float numbers(){
+			float harmonic = 1;
+			float num = 1;
+			   Scanner number = new Scanner(System.in);
+			   System.out.println("enter any number to display the sum of  numbers in harmonic form");
+			   int n = number.nextInt();
+			   
+			  for (int j=1; j<=n;j++) {
+				  
+				  harmonic = harmonic+(float)1/j;
+				  System.out.println("sum of the harmonic numbers until "+(num/j)+" is "+harmonic);
+			  }return harmonic;
+		  }
+		   public static void main(String[] args) {
+				new HarmonicSum().numbers();  
+		   
+		   }
+	
+}
